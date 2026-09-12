dad6d91d492e83f0e00d8b7e435b52969074fd0c
madhanmathew22-code <madhanmathew22@gmail.com>
2026-09-13 03:16:15 +0530

    Update HabitFlowNavHost.kt

--- HabitFlow/app/src/main/java/com/habitflow/app/ui/navigation/HabitFlowNavHost.kt
+++ HabitFlow/app/src/main/java/com/habitflow/app/ui/navigation/HabitFlowNavHost.kt
@@ -1,5 +1,5 @@
 package com.habitflow.app.ui.navigation
-
+import androidx.compose.foundation.layout.weight
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
